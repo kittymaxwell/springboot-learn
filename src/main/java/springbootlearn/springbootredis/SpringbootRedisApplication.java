6abe45a4a6891c20edf95c7f4b10324b154b0dac
wujianbin <1303791589@qq.com>
2020-09-19 10:16:48 +0800

    Dockerfile构建项目

--- src/main/java/springbootlearn/springbootredis/SpringbootRedisApplication.java
+++ src/main/java/springbootlearn/springbootredis/SpringbootRedisApplication.java
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @MapperScan("springbootlearn.springbootredis.dao")
 @EnableCaching
-@EnableScheduling
+//@EnableScheduling
 public class SpringbootRedisApplication {
 
     public static void main(String[] args) {
